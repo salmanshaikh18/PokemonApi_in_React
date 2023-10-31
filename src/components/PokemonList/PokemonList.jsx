54842aa3ef59c8b6395d8f6b278dfd1fc3d0d299
Salman <salmanshaikh18786@gmail.com>
2023-10-31 08:01:40 +0530

    api logic

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -9,15 +9,26 @@ const PokemonList = () => {
 
   const [pokemonList, setPokemonList] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
   const downloadPokemons = async () => {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-    const pokemonResults = response.data.results
-    const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
+    const response = await axios.get(POKEDEX_URL) // this downloads list of 20 pokemons
+    console.log("response.data: ", response.data) 
+
+    const pokemonResults = response.data.results // we get the array of pokemons (name, link for details of pokemons) from result
+    console.log('pokemonResults: ', pokemonResults)
+
+    // iterating over the array of pokemons, and using their url, to create an array of promises 
+    // that will download those 20 pokemons
+    const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url)) // array of 20 pokemon detailed data
     console.log("hey: ", pokemonResultPromise)
+
+    // passing that promise array to axios.all
     const pokemonData = await axios.all(pokemonResultPromise)
     console.log(pokemonData)
-    const result = pokemonData.map((pokeData) => {
+
+    // now iterate on the data of each pokemon, and extract id, name, image, types
+    const pokeLIstResult = pokemonData.map((pokeData) => {
       const pokemon = pokeData.data;
       return { 
         id: pokemon.id,
@@ -25,10 +36,10 @@ const PokemonList = () => {
         image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny, 
         types: pokemon.types }
     })
-    console.log(result)
-    setPokemonList(result)
+    console.log(pokeLIstResult)
+
+    setPokemonList(pokeLIstResult)
     setIsLoading(false)
-    // console.log(response.data.results)
   }
 
   useEffect(() => {
