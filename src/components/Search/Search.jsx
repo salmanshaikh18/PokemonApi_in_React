80d4cac188b898e1dc31a4feeffe9e6b2ce30920
Salman <salmanshaikh18786@gmail.com>
2023-11-04 01:14:35 +0530

    correction

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,7 +3,7 @@ import './Search.css'
 
 const Search = () => {
   const handleInput = () => {
-    alert('This feature is not implemented yet, This will coming soon. Expore other features like: Scroll down and click on next button to get next 20 pokemons and you can also click on any pokemon to see their details')
+    alert('This feature is not implemented yet, This will coming soon. Explore other features like: Scroll down and click on next button to get next 20 pokemons and you can also click on any pokemon to see their details')
   }
   return (
     <div className='search-wrapper'>
